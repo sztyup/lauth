602f21ba1208db1db8989312967c250689b55e17
sztyup <sztyup@gmail.com>
2019-12-29 14:15:39 +0100

    Exception improvements

--- src/LAuth/Exceptions/InvalidStateException.php
+++ src/LAuth/Exceptions/InvalidStateException.php
@@ -5,7 +5,7 @@ namespace Sztyup\LAuth\Exceptions;
 
 class InvalidStateException extends LauthException
 {
-    public function __construct(string $expected, string $received)
+    public function __construct(?string $expected, ?string $received)
     {
         parent::__construct(sprintf('Expected state: %s, received: %s', $expected, $received));
     }
--- src/LAuth/Exceptions/ProviderNotFound.php
+++ src/LAuth/Exceptions/ProviderNotFound.php
@@ -3,10 +3,12 @@ declare(strict_types=1);
 
 namespace Sztyup\LAuth\Exceptions;
 
+use Throwable;
+
 class ProviderNotFound extends LauthException
 {
-    public function __construct(string $provider)
+    public function __construct(string $provider, Throwable $previous = null)
     {
-        parent::__construct(sprintf('Provider [%s] not found', $provider));
+        parent::__construct(sprintf('Provider [%s] not found', $provider), 0, $previous);
     }
 }
